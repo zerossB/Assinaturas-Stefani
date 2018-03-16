1f7170ce4b85151bd1e6e3f86d7f443387c6286a
Guilherme Haynes Howe <guilherme@stefani.local>
2018-03-16 11:55:23 -0300

    Manças de Logos

--- min.py
+++ min.py
@@ -1,8 +1,8 @@
-from PIL import Image
-from PIL import ImageFont
-from PIL import ImageDraw
+from PIL import Image, ImageFont, ImageDraw, ImageFilter
+from openpyxl import load_workbook
 
 COLOR = (23, 103, 200)
+COR_BG = (255, 255, 255)
 
 # font = ImageFont.truetype(<font-file>, <font-size>)
 fontBold = ImageFont.truetype("Roboto-Bold.ttf", 16)
@@ -11,23 +11,41 @@ fontBNormal = ImageFont.truetype("Roboto-Regular.ttf", 14)
 fontNormal = ImageFont.truetype("Roboto-Regular.ttf", 12)
 fontNBold = ImageFont.truetype("Roboto-Bold.ttf", 12)
 
-img = Image.open("Ass-Email.jpg")
-draw = ImageDraw.Draw(img)
 
-# draw.text((x, y),"Sample Text",(r,g,b))
-draw.text((215, 8), "Guilherme Haynes Howe", COLOR, font=fontBold)
-draw.text((215, 29), "Técnico em Informática", COLOR, font=fontBNormal)
+wb = load_workbook(filename='xlsx\Assinaturas.xlsx')
+sheet = wb['Sheet1']
 
-draw.text((215, 55), "Phone:", COLOR, font=fontNBold)
-draw.text((280, 55), "+55 (16) 3209-4788", COLOR, font=fontNormal)
+for key, cell in enumerate(sheet):
+    #img = Image.open("Ass-Email.jpg")
+    img = Image.new('RGB', (500, 135), (255, 255, 255))
+    draw = ImageDraw.Draw(img)
 
-draw.text((215, 73), "Email:", COLOR, font=fontNBold)
-draw.text((280, 73), "tec@ceramicastefani.com.br", COLOR, font=fontNormal)
+    # draw.text((x, y),"Sample Text",(r,g,b))
+    draw.text((215, 8), cell[0].value, COLOR, font=fontBold)
+    draw.text((215, 29), cell[1].value, COLOR, font=fontBNormal)
 
-draw.text((215, 91), "Skype:", COLOR, font=fontNBold)
-draw.text((280, 91), "haynes_g", COLOR, font=fontNormal)
+    stefani = Image.open("Logo-Stefani.jpg")
+    stefani = stefani.resize((128, 120), Image.ANTIALIAS)
+    img.paste(stefani, (40, 7))
 
-draw.text((215, 109), "Facebook:", COLOR, font=fontNBold)
-draw.text((280, 109), "facebook.com/CeramicaStefaniSA", COLOR, font=fontNormal)
+    draw.line([(205, 5), (205, 130)], fill=COLOR)
+    draw.line([(215, 50), (490, 50)], fill=COLOR)
 
-img.save('sample-out.jpg')
+    draw.text((215, 55), "Phone:", COLOR, font=fontNBold)
+    draw.text((280, 55), "+55 (16) 3209-4788", COLOR, font=fontNormal)
+
+    draw.text((215, 73), "Email:", COLOR, font=fontNBold)
+    draw.text((280, 73), cell[2].value, COLOR, font=fontNormal)
+
+    if cell[3].value is None:
+        draw.text((215, 91), "Facebook:", COLOR, font=fontNBold)
+        draw.text((280, 91), "facebook.com/ceramicastefanisa",
+                  COLOR, font=fontNormal)
+    else:
+        draw.text((215, 91), "Skype:", COLOR, font=fontNBold)
+        draw.text((280, 91), cell[3].value, COLOR, font=fontNormal)
+        draw.text((215, 109), "Facebook:", COLOR, font=fontNBold)
+        draw.text((280, 109), "facebook.com/ceramicastefanisa",
+                  COLOR, font=fontNormal)
+
+    img.save("jpg/"+cell[2].value + '.jpg')
